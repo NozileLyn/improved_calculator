2f5aba94fb5f2bb6dbe9ec5cb61a8996575f5262
NozileLyn <lynmlilo0@gmail.com>
2023-07-20 15:25:50 +0100

    Intial Commit

---
+++ Lyn Second Calculator Attempt.py
@@ -0,0 +1,20 @@
+#Lyn's Simple Calculator
+first_number = int(input("Enter your first number"))
+second_number =int(input("Enter your second number"))
+
+operation = input ("Enter the operation (+,-,*,/)")
+
+if operation == "*":
+    result = first_number * second_number
+    print("Result",result)
+elif operation == "-":
+    result = first_number - second_number
+    print("Result", result)
+elif operation == "+":
+    result = first_number + second_number
+    print("Result", result)
+elif operation = "/":
+    result = first_number / second_number
+    print("Result", result)
+elif:
+    print ("Invalid")
---
+++ main.py
@@ -0,0 +1,11 @@
+#Simple Calculator
+first_number = int(input("Enter your first number"))
+second_number =int(input("Enter your second number"))
+
+operation = input ("Enter the operation (+,-,*,/)")
+
+if operation == "*":
+    result = first_number * second_number
+    print("Result",result)
+else:
+    print("Invalid")
